1e92f29fec8371ca29bd91e28778a0672b8ed34e
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-05-02 23:28:59 -0300

    comments

--- go_by_example/01_hello_world/hello_world.go
+++ go_by_example/01_hello_world/hello_world.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
+// um programa em go precisa de um package main uma func main, esse é o entrypoint da aplicação
 func main() {
-	// rode usando go run <filepath>
-	// para realizar o build é necessário rodar go build <filepath>
+	// $ go run <filepath>
+	// $ go build <filepath> && ./<binary>
 	// fmt é o pacote da stdlib usado para lidar com formatação de I/O
 	fmt.Println("hello world!")
 }
