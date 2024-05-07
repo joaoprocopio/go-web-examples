01197bf92c8c92b321fec4e8cb574d98a54e2166
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-05-06 23:05:22 -0300

    rewrite

--- go_by_example/01_hello_world/hello_world.go
+++ go_by_example/01_hello_world/hello_world.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 // a Go application needs a `package main` and a `func main` in order to execute
-// this is the entrypoint of your application
+// it is the entrypoint of your application
 func main() {
-	// $ go run <filepath>
-	// $ go build <filepath> && ./<binary>
-	// fmt é o pacote da stdlib usado para lidar com formatação de I/O
+	// this command is used to compiled and directly run a Go file
+	// $ go run hello_world.go
+
+	// these commands are used to build and execute Go code
+	// $ go build hello_world.go
+	// $ ./hello_world
+
+	// fmt is a standard library package that handles I/O formattings
 	fmt.Println("hello world!")
 }
