2b8898cc5a70a7aeb092df0eeccbbf94cf444535
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-05-08 22:57:11 -0300

    slices

--- go_by_example/09_slices/slices.go
+++ go_by_example/09_slices/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 // slices are an important data type in Go
 // giving a more powerful interface to sequences than arrays
@@ -33,4 +36,52 @@ func main() {
 	// slices support several more that make them richer than arrays
 	// one is the builtin append, which returns a slice containing one or more new values
 	// note that we need to accept a return value from append as we may get a new slice value
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	// slices can aso be copy'd
+	// here we create an empty slice c of the same length as s and copy into c from s
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	// slices support a "slice" operator with the syntax slice[low:high]
+	// for example, this gets a slice of elements s[2], s[3] and s[4]
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	// the slices up to (but excluding s[5])
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	// and slices up from (and including) s[2]
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	// we can declare and initialize a variable for slice in a single line as well
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	// the slices package contains a number of useful utility functions for slices
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+
+	// slices can be composed into multi-dimensional data structures
+	// the length of the inner slices can vary, unlike multi-dimensional arrays
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
+
+	// note that while slices are different types than arrays
+	// they are rendered similarly by fmt.Println
+	// checkout this blog post about slices https://go.dev/blog/slices-intro
 }
