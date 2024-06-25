4465efec5c86d1e8f9e44d77a5970cd1ee2700e2
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-06-25 00:06:04 -0300

    channels note

--- go_by_example/28_channels/channels.go
+++ go_by_example/28_channels/channels.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // channels are the pipes that connect concurrent goroutines
 // you can send values into channels from one goroutine and receive those values into another goroutine
@@ -13,10 +16,17 @@ func main() {
 	// send a new value int oa channel using the channel <- syntax
 	// here we send "ping" to the messages channel we made above
 	// from a new goroutine
-	go func() { messages <- "ping" }()
+	go func() {
+		time.Sleep(time.Second * 5)
+		messages <- "ping"
+	}()
 
 	// the <-channel syntax receives a value from the channel
 	// here we'll receive the "ping" message we sent above and print it out
+	// note: 	this syntax makes the current execution blocks until the channel receives a value
+	// 			if the coroutine that you're channeling to doesn't receives a value
+	// 			the current execution deadlocks
+	// 			https://www.craig-wood.com/nick/articles/deadlocks-in-go/
 	msg := <-messages
 	fmt.Println(msg)
 }
